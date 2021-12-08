675f411a410252591436813d4996887ab8433dd1
Ryan Helgerson <ryan.helgerson@gmail.com>
2021-12-08 15:57:01 -0600

    checking time of day to render colors on time blocks

--- script.js
+++ script.js
@@ -8,5 +8,42 @@ $(document).ready(function() {
 
         console.log(time);
         console.log(value);
-    })
+
+        //save in localStorage
+        localStorage.setItem(time, value);
+
+        //Show notification that item was saved to localStorage by adding 'show' class
+        $('.notification').addClass('show');
+
+        //Timeout to remove 'show' class after 5 seconds
+        setTimeout(function() {
+            $('.notification').removeClass('show');
+        }, 5000);
+    });
+
+    function updateTime() {
+        //get current number of hours
+        var currentHour = moment().hours();
+        console.log(currentHour);
+
+        //loop over time blocks
+        $('.time-block').each(function () {
+            var blockHour = parseInt($(this).attr('id').split('-')[1]);
+            console.log(blockHour);
+
+            //check whether the block is in the past, present or future
+            if (blockHour < currentHour){
+                $(this).addClass('past');
+            } else if (blockHour === currentHour) {
+                $(this).removeClass('past');
+                $(this).addClass('present');
+            } else {
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).addClass('future');
+            }
+        });
+    }
+
+    updateTime();
 })
