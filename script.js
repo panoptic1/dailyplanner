17e6569d51437a0a8b2f14b629c2605f4624150c
Ryan Helgerson <ryan.helgerson@gmail.com>
2021-12-08 20:43:19 -0600

    app working

--- script.js
+++ script.js
@@ -46,4 +46,25 @@ $(document).ready(function() {
     }
 
     updateTime();
+
+    // set up interval to check if current time needs to be updated
+    var interval = setInterval(updateTime, 15000);
+
+    //load any saved data from local storage
+    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+    $('#hour-18 .description').val(localStorage.getItem('hour-18'));
+    $('#hour-19 .description').val(localStorage.getItem('hour-19'));
+    $('#hour-20 .description').val(localStorage.getItem('hour-20'));
+    $('#hour-21 .description').val(localStorage.getItem('hour-21'));
+
+    //display current day
+    $('#current-day').text(moment().format('dddd, MMMM Do'));
 })
